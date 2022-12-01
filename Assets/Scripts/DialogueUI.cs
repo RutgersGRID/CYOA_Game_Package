9d3dee97fadf2184b78303abc3000e92d247b65a
Eric <98132867+HornOfSaltff@users.noreply.github.com>
2022-12-01 17:03:26 -0500

    Journal + 2 option answers + 3 option answers
    answer buttons are not working
    NullReferenceException: Object reference not set to an instance of an object
    DialogueUI.OnEnable () (at Assets/Scripts/DialogueUI.cs:105)

--- Assets/Scripts/DialogueUI.cs
+++ Assets/Scripts/DialogueUI.cs
@@ -6,7 +6,10 @@ using UnityEngine.SceneManagement;
 
 public class DialogueUI : MonoBehaviour
 {
-/// Dialogue section    
+/// Dialogue section   
+    
+    private VisualElement _dBox;
+
     public string[] names;
     public string[] sentences;
     public Sprite[] characters;
@@ -15,38 +18,154 @@ public class DialogueUI : MonoBehaviour
     private Button _dialogueButton;
     private Label _nameLabel;
     private Label _dialogueLabel;
-///
 
-/// Character expression and props
     private VisualElement _characterVE;
     private VisualElement _propVE;
 ///
 
-                               
-    private int count;
+/// Journal Button
+    private Button _journalButton;
+    private VisualElement _journalUIVE;
+    private Button _exitJournalButton;
+///
+
+/// Rewind Button
+    private Button _rewindButton;
+///
+
+/// Two answers
+    private VisualElement _twoAnswers;
+    private Button _taanswerA;
+    private Button _taanswerB;
+///
+
+/// Three answers
+    private VisualElement _threeAnswers;
+    private Button _answerA;
+    private Button _answerB;
+    private Button _answerC;
+///
+
+/// Array Counter                       
+    private int count = 0;
+///
+
+
     // Start is called before the first frame update
     private void OnEnable()
     {
         var rootVisualElement = GetComponent<UIDocument>().rootVisualElement;
+
+        /// Dialogue
+        _dBox = rootVisualElement.Q<VisualElement>("DBox");
         _nameLabel = rootVisualElement.Q<Label>("dlog-name");
         _dialogueLabel = rootVisualElement.Q<Label>("dlog-text");
         _dialogueButton = rootVisualElement.Q<Button>("dlog-button");
-
         _characterVE = rootVisualElement.Q<VisualElement>("Characters");
         _propVE = rootVisualElement.Q<VisualElement>("Props");
+        ///
 
+        /// Journal
+        _journalButton = rootVisualElement.Q<Button>("journal-button");
+        _journalUIVE = rootVisualElement.Q<VisualElement>("JournalUIContainer");
+        _exitJournalButton = rootVisualElement.Q<Button>("exit-ui-button");
+        _journalUIVE.style.display = DisplayStyle.None;
+        ///
+
+        /// Rewind
+        _rewindButton = rootVisualElement.Q<Button>("rewind-button");
+        ///
+
+        /// Two answers
+        _twoAnswers = rootVisualElement.Q<VisualElement>("answer-two-options");
+        _twoAnswers.style.display = DisplayStyle.None;
+        _taanswerA = rootVisualElement.Q<Button>("answer-a");
+        _taanswerB = rootVisualElement.Q<Button>("answer-b");
+        ///
+
+        /// Three answers
+        _threeAnswers = rootVisualElement.Q<VisualElement>("answer-three-options");
+        _threeAnswers.style.display = DisplayStyle.None;
+        _answerA = rootVisualElement.Q<Button>("answer-a-t");
+        _answerB = rootVisualElement.Q<Button>("answer-b-t");
+        _answerC = rootVisualElement.Q<Button>("answer-c-t");
+        ///
+
+        /// Button click events
         _dialogueButton.RegisterCallback<ClickEvent>(ev => dialogueLabel());
+
+        _journalButton.RegisterCallback<ClickEvent>(ev => OpenJournal());
+        _exitJournalButton.RegisterCallback<ClickEvent>(ev => CloseJournal());
+
+        _rewindButton.RegisterCallback<ClickEvent>(ev => Rewind());
+
+        //_taanswerA.RegisterCallback<ClickEvent>(ev => GoToA());
+        //_taanswerB.RegisterCallback<ClickEvent>(ev => GoToB());
+
+        _answerA.RegisterCallback<ClickEvent>(ev => GoToA());
+        _answerB.RegisterCallback<ClickEvent>(ev => GoToB());
+        _answerC.RegisterCallback<ClickEvent>(ev => GoToC());
+        // _answerA.RegisterCallback<ClickEvent>(ev => OpenJournal());
+        // _answerB.RegisterCallback<ClickEvent>(ev => OpenJournal());
+        // _answerC.RegisterCallback<ClickEvent>(ev => OpenJournal());
+        ///
     }
 
+    /// Dialogue system method
     private void dialogueLabel()
     {
-        _nameLabel.text = names[count];
-        _dialogueLabel.text = sentences[count];
-        _characterVE.style.backgroundImage = new StyleBackground(characters[count]);
-        _propVE.style.backgroundImage = new StyleBackground(props[count]);
-        count++;
+        if(count >= names.Length)
+        {
+            _dBox.style.display = DisplayStyle.None;
 
+            //_twoAnswers.style.display = DisplayStyle.None;
+            _threeAnswers.style.display = DisplayStyle.Flex;
+
+        }
+        if(count < names.Length)
+        {
+            _nameLabel.text = names[count];
+            _dialogueLabel.text = sentences[count];
+            _characterVE.style.backgroundImage = new StyleBackground(characters[count]);
+            _propVE.style.backgroundImage = new StyleBackground(props[count]);
+        }
+        
+        count++;
     }
+    ///
+
+    /// Rewind Method
+    private void Rewind()
+    {
+        SceneManager.LoadScene("PrototypeUITools");
+    }
+    ///
+
+    /// Journal Methods
+    private void OpenJournal()
+    {
+        _journalUIVE.style.display = DisplayStyle.Flex;
+    }
+    private void CloseJournal()
+    {
+        _journalUIVE.style.display = DisplayStyle.None;
+    }
+    ///
+
+    /// Answers method
+    private void GoToA()
+    {
+        SceneManager.LoadScene("PickA");
+    }
+    private void GoToB()
+    {
+        SceneManager.LoadScene("PickB");
+    }
+    private void GoToC()
+    {
+        SceneManager.LoadScene("PickC");
+    }
+    ///
 
     void start()
     {
