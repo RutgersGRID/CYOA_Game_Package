a54f8f6944044981d0d7ab197e3eb6e394711d60
Eric <98132867+HornOfSaltff@users.noreply.github.com>
2023-02-06 21:48:00 -0500

    Missing files fixed
    Some files were missing from the Asset and Library folder.

--- Assets/Scripts/ReadCSV.cs
+++ Assets/Scripts/ReadCSV.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+
 public class ReadCSV : MonoBehaviour
 {
     public TextAsset textAssetData;
@@ -34,7 +35,7 @@ public class ReadCSV : MonoBehaviour
 
     void ReadCSVFile()
     {
-        string[] data = textAssetData.text.Split(new string[] { ",", "\n"}, StringSplitOptions.None);
+        string[] data = textAssetData.text.Split(new string[] { ",", "\n"}, StringSplitOptions.None );
 
         int tableSize = data.Length / 4 - 1;
         myCharacterList.character = new Character[tableSize];
