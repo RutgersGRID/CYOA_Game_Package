408ca96d8b3003efda380bc4675c8da671b0bc59
Eric <98132867+HornOfSaltff@users.noreply.github.com>
2024-07-11 10:14:00 -0400

    Match Accesscodes to story

--- Assets/Scripts/AccessControl/GoogleSheetReaderTwo.cs
+++ Assets/Scripts/AccessControl/GoogleSheetReaderTwo.cs
@@ -15,7 +15,8 @@ public class GoogleSheetReaderTwo : MonoBehaviour
     }
 
     public List<LoginSO> logins = new List<LoginSO>();
-    private const string CONTROL_SHEET_URL = "https://sheets.googleapis.com/v4/spreadsheets/1Q793Lo8hsyxlNvFJT8V1bGSYP9yWYY8PU8TeGaGU-w8/values/Access?key=AIzaSyDxlgY5nx2_JX89Grs3KZ7cnxlpRO2Nedg";
+    //private const string CONTROL_SHEET_URL = "https://sheets.googleapis.com/v4/spreadsheets/1Q793Lo8hsyxlNvFJT8V1bGSYP9yWYY8PU8TeGaGU-w8/values/Access?key=AIzaSyDxlgY5nx2_JX89Grs3KZ7cnxlpRO2Nedg";
+    private const string CONTROL_SHEET_URL = "https://sheets.googleapis.com/v4/spreadsheets/1SLm9j993IbtSKpzmVoshhebh7FxJcZOp2a4BU5aId8g/values/Access?key=AIzaSyDxlgY5nx2_JX89Grs3KZ7cnxlpRO2Nedg";
 
     public delegate void OnDataLoaded();
     public event OnDataLoaded onDataLoaded;
